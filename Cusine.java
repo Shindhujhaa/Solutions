f05564589dd1605d3a4bb74c58433ce75811aba9
Shindhujhaa <64688179+Shindhujhaa@users.noreply.github.com>
2020-05-02 16:57:49 +0530

    obj creation done

--- Cusine.java
+++ Cusine.java
@@ -3,6 +3,20 @@ class Main()
 {
 public Static void main(String a[])
 {
+  Main o=new Main();
 System.out.println("welcome");
-}
-}
+ System.out.println(" Select 1.Asian  2.Italian");
+Scanner sc=new Scanner(System.in);
+  int n=sc.nextIn();
+  if (n==1)
+  {
+    o.asian();
+  }
+  else 
+  {
+    a.italian();
+  }
+}}
+  
+    
+  
